51600e16b53ac064a9c7d5f22a33f302e4bb6707
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-10-05 15:10:16 +0300

    Create dz4.asm

---
+++ ДЗ №4/dz4.asm
@@ -0,0 +1,68 @@
+.data
+sep:    .asciz  "--------\n"
+prompt: .asciz  "Максимальный допустимый аргумент факториала: "
+prompt1: .asciz  ", его факториал: "
+ln:     .asciz "\n"
+
+.text
+main:
+        li      a1, 1
+
+loop:
+    	mv      a0, a1
+        jal     fact
+        
+        li      t0, 1000
+        bgeu    a0, t0, end
+    	
+        li      a7, 1
+        ecall
+        
+        li      a7, 4
+        la      a0, ln
+        ecall
+        
+        addi    a1, a1, 1
+        j       loop
+
+end:
+	li      a7, 4
+        la      a0, sep
+        ecall
+        
+        li      a7, 4
+        la      a0, prompt
+        ecall
+        
+	addi	a0, a1, -1
+	li      a7, 1
+        ecall
+        
+        li      a7, 4
+        la      a0, prompt1
+        ecall
+        
+        addi	a0, a1, -1
+        jal     fact
+	li      a7, 1
+        ecall
+        
+        li      a7, 10
+        ecall
+
+fact:   addi    sp sp -8
+        sw      ra 4(sp)
+        sw      s1 (sp)
+
+        mv      s1 a0
+        addi    a0 s1 -1
+        li      t0 1
+        ble     a0 t0 done
+        jal     fact
+        mul     s1 s1 a0
+
+done:   mv      a0 s1
+        lw      s1 (sp)
+        lw      ra 4(sp)
+        addi    sp sp 8
+        ret
