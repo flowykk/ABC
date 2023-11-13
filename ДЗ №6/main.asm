0c84185663a04e3469de2d059b3c985a820b484f
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-13 15:50:58 +0300

    Update main.asm

--- ДЗ №6/main.asm
+++ ДЗ №6/main.asm
@@ -1,88 +1,32 @@
-.eqv    SIZE 100                # размер буфера
-.data
-main_str:    .space SIZE             # буфер
-new_str:     .space SIZE
+.include "macrolib.asm"
+
+.data 
+empty_string_prompt: 	.asciz "Копирование пустой строки "
+empty_str:    .asciz "       "
+
+test_string_prompt: 	.asciz "Копирование строки средней длины"
 test_str:    .asciz "test string"
-prompt1:  .asciz  "Введите строку: "
-prompt3:  .asciz  "Скопированная строка: " 
-ln:     .asciz "\n"
 
-.macro PrintPrompt(%x)
-	la 	a0 %x    
-	li 	a7 4           
-        ecall
-.end_macro
-
-.macro PrintNumber(%x)
-	li      a7 1
-        mv      a0 %x       
-        ecall
-.end_macro
-
-.macro InputNumber(%x)
-	li      a7 5
-        ecall
-        mv      %x a0
-.end_macro
-
-.macro CreateString(%string_address, %x)
-	la      a0 %string_address
-        mv 	%x a0
-        mv 	t5 %x
-.end_macro
-
-.macro ParseTestString(%string_address, %x)
-	      la      a0 %string_address
-        li      a1 SIZE
-        mv 	    %x a0
-.end_macro
-
-.macro InputString(%string_address, %x)
-	      la      a0 %string_address
-        li      a1 SIZE
-        li      a7 8
-        ecall
-        mv 	    %x a0
-.end_macro
-
-.macro PrintString(%x)
-      	mv 	    a0 %x
-      	li 	    a7 4
-      	ecall
-.end_macro
-
-.macro Exit()
-	      li      a7 10
-        ecall
-.end_macro
+long_string_prompt: 	.asciz "Копирование длинной строки "
+long_str: 	   .asciz "long long long long long long long string"
 
 .text
+.global main
 main:
       	PrintPrompt(prompt1)
       	InputString(main_str, t3)
-      
       	jal strcpy
       	
-      	ParseTestString(test_str, t3)
-        jal strcpy
+      	PrintPrompt(empty_string_prompt)
+      	PrintPrompt(ln)
+        strcpy(empty_str, t3)
+        
+        PrintPrompt(test_string_prompt)
+      	PrintPrompt(ln)
+        strcpy(test_str, t3)
+        
+        PrintPrompt(long_string_prompt)
+      	PrintPrompt(ln)
+	strcpy(long_str, t3)
       	
       	Exit()
-
-strcpy:   
-	      CreateString(new_str, t4)
-	
-loop:
-      	lb      t1 (t3)
-      	sb	    t1 (t4)
-        beqz    t1 end
-        
-strcpy_next:   
-      	addi    t3 t3 1
-      	addi    t4 t4 1
-        b       loop
-        
-end:
-	PrintPrompt(prompt3)
-	PrintString(t5)
-
-ret
