b45c5e58882250d9f21b2558a14a467d66c475f2
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-21 15:06:42 +0300

    Create main.asm

---
+++ ИДЗ №3/main.asm
@@ -0,0 +1,20 @@
+.include "macrolib.asm"
+
+.text
+.global main
+# /Users/danila/asmProjects/ihw3/test.txt
+main:       
+	  jal read_from_file
+
+	  PrintPrompt(ln)
+    PrintPrompt(read_str)
+    PrintString(t5)
+
+  	dotask(t5)
+  	
+  	PrintPrompt(ln)
+  	PrintPrompt(result_str)
+  	PrintString(t3)
+  	
+  	Exit()
+
