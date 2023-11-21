659c3b3251c2e950f6dbeb078285471e1bb7f24d
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-21 15:44:23 +0300

    Update main.asm

--- ИДЗ №3/main.asm
+++ ИДЗ №3/main.asm
@@ -4,17 +4,17 @@
 .global main
 # /Users/danila/asmProjects/ihw3/test.txt
 main:       
-	  jal read_from_file
+	jal read_from_file
 
-	  PrintPrompt(ln)
-    PrintPrompt(read_str)
-    PrintString(t5)
+	PrintPrompt(ln)
+    	PrintPrompt(read_str)
+    	PrintString(t5)
 
   	dotask(t5)
   	
-  	PrintPrompt(ln)
   	PrintPrompt(result_str)
   	PrintString(t3)
   	
+  	jal out_in_file
+  	
   	Exit()
-
