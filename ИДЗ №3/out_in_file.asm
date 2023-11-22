45c406791056a1d321e26c8f36fc79e94d4393c7
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-22 19:32:12 +0300

    Update out_in_file.asm

--- ИДЗ №3/out_in_file.asm
+++ ИДЗ №3/out_in_file.asm
@@ -3,52 +3,54 @@
 .data
 default_name: .asciz "testout.txt"      # Имя файла по умолчанию
 # Это выводимый текст
-buffer: .asciz "The quick brown fox jumps over the lazy dog."
 
 .text
 .global out_in_file
 out_in_file:
+	# Вывод подсказки
 	PrintPrompt(prompt2)
-	###############################################################
+
 	# Ввод имени файла с консоли эмулятора
 	la	a0 file_name
 	li      a1 NAME_SIZE
 	li      a7 8
 	ecall
+	
 	# Убрать перевод строки
 	li  	t4 '\n'
 	la  	t5 file_name
-	mv  	t3 t5	# Сохранение начала буфера для проверки на пустую строку
+	mv  	t3 t5
+	
 loop:
 	lb	t6 (t5)
 	beq 	t4 t6 replace
 	addi 	t5 t5 1
 	b   loop
+	
 replace:
 	beq 	t3 t5 default	# Установка имени введенного файла
 	sb  	zero (t5)
-	mv  	a0 t3 	# Имя, введенное пользователем
+	mv  	a0 t3 		# Имя, введенное пользователем
 	b out
 default:
 	la   	a0 default_name # Имя файла по умолчани
-	###############################################################
+
 out:
 	# Open (for writing) a file that does not exist
-	li   	a7 1024     # system call for open file
-	li   	a1 1        # Open for writing (flags are 0: read, 1: write)
-	ecall             # open a file (file descriptor returned in a0)
-	mv   	s6 a0       # save the file descriptor
-	###############################################################
+	li   	a7 1024     	# system call for open file
+	li   	a1 1        	# Open for writing (flags are 0: read, 1: write)
+	ecall             	# open a file (file descriptor returned in a0)
+	mv   	s6 a0       	# save the file descriptor
+
 	# Write to file just opened
-	li   	a7 64       # system call for write to file
-	mv   	a0 s6       # file descriptor
-	mv   	a1 a6   # address of buffer from which to write
-	li   	a2 44       # hardcoded buffer length
-	ecall             # write to file
-	###############################################################
+	li   	a7 64       	# system call for write to file
+	mv   	a0 s6       	# file descriptor
+	mv   	a1 a6   	# address of buffer from which to write
+	li   	a2 44       	# hardcoded buffer length
+	ecall             	# write to file
+
 	# Close the file
-	li   	a7 57       # system call for close file
-	mv   	a0 s6       # file descriptor to close
-	ecall             # close file
-	###############################################################
+	li   	a7 57       	# system call for close file
+	mv   	a0 s6       	# file descriptor to close
+	ecall             	# close file
 ret
