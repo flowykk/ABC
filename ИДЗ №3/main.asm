72f54a711df0978497d1ea3ac136cfa6e21a2c85
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-22 19:31:23 +0300

    Update main.asm

--- ИДЗ №3/main.asm
+++ ИДЗ №3/main.asm
@@ -2,19 +2,32 @@
 
 .text
 .global main
-# /Users/danila/asmProjects/ihw3/test.txt
+
 main:       
+	# Вызов подпрограммы для выгрузки строки из файла
 	jal read_from_file
 
-	PrintPrompt(ln)
+	# Вывод вывод содержания файла
     	PrintPrompt(read_str)
     	PrintString(t5)
 
-  	dotask(t5)
+	# Вызов подпрограммы для обработки строки
+  	StringProcessing(t5)
   	
-  	PrintPrompt(result_str)
+  	# Возможность пользователя выбрать вывести результат или нет
+  	UserDecision(t1)
+  	bgtz a0 print_result
+  	j no_result
+
+# Пользователь хочет видеть результат
+print_result:
+  	# Вывод результата
+  	#PrintPrompt(result_str)
   	PrintString(t3)
-  	
+
+no_result:
+  	# Запись результата в файл
   	jal out_in_file
   	
+  	# Завершение работы программы
   	Exit()
