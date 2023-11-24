50b29a04102528239cc6699f8607f134a9e2eb0d
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-11-24 21:25:47 +0300

    Update macrolib.asm

--- ИДЗ №3/macrolib.asm
+++ ИДЗ №3/macrolib.asm
@@ -7,13 +7,23 @@ new_str:     .space SIZE
 user_dec:    .space SIZE
 
 ln:     	.asciz "\n"
+res:		.asciz "Результат: "
 read_str:     	.asciz "Строка, прочитнная из файла: "
 result_str:	.asciz "Хотите ли вы увидеть результат?\nЕсли да, введите Y, В противном случае введите любой другой символ: "
 prompt:         .asciz "Введите путь к файлу, откуда считать данные: "
 prompt2:        .asciz "Введите путь к файлу, куда записать данные: "
 er_name_mes:    .asciz "Указан некорректный путь к файлу!\n"
 er_read_mes:    .asciz "Произошла ошибка во время чтения из файла!\n"
+success:    	.asciz "Запись успешно произошла!"
 file_name:      .space	NAME_SIZE
+
+test1: 		.asciz "Тест №1\n"
+test2: 		.asciz "Тест №2\n"
+test3: 		.asciz "Тест №3\n"
+test4: 		.asciz "Тест №4\n"
+test5: 		.asciz "Тест №5\n"
+
+test1_file: .asciz "/Users/danila/asmProjects/ihw3/TestData/test1_out.txt"
 
 # Макрос для проверки, является ли текущий символ строки цифрой
 # x - текущий символ строки
@@ -34,19 +44,18 @@ end:
 
 # Макрос возможности пользователя выбрать выводить результат или нет
 .macro UserDecision(%x)
-	PrintPrompt(result_str)
-	
-	InputString(user_dec, %x)
+	InputString(result_str, user_dec, SIZE, %x)
 	lb      t4 (%x) 
 	li 	t2 89
 	beq	t4 t2 yes_branch
-	li 	a0 0
+	li 	a3 0
 	j end
 	
 yes_branch:
-	li 	a0 1
+	li 	a3 1
 
 end: 
+
 .end_macro
 
 # Макрос для обработки строки в соответствии с условием задания
@@ -86,14 +95,17 @@ end:
 .end_macro
 
 # Макрос для ввода строки
-# string_address - адрес будущей строки 
+# message - адрес строкм  с сообщением для пользователя
+# address - адрес будущей строки 
+# size - размер вводимой строки
 # x - регистр, где будетз аписан адрес первого символа строки
-.macro InputString(%string_address, %x)
-	la      a0 %string_address
-        li      a1 SIZE
-        li      a7 8
+.macro InputString(%message, %address, %size, %x)
+	la      a0 %message
+	la 	a1 %address
+        li      a2 %size
+        li      a7 54
         ecall
-        mv 	%x a0
+        la 	%x %address
 .end_macro
 
 # Макрос для создания строки
@@ -114,10 +126,14 @@ end:
 .end_macro
 
 # Макрос для вывода строк
-# x - регистр со строкой, которую нужно вывести в консоль
-.macro PrintString(%x)
-      	mv 	a0 %x
-      	li 	a7 4
+# message - адрес строкм  с сообщением для пользователя
+# address - адрес строки, которую надо показать пользователю 
+.macro PrintString(%message, %address)
+	la 	a0 %message
+	mv 	a1 %address
+      	li 	a7 59
+      	#mv 	a0 %x
+      	#li 	a7 4
       	ecall
       	PrintPrompt(ln)
 .end_macro
@@ -128,6 +144,38 @@ end:
 	li      a7 1
         mv      a0 %x       
         ecall
+.end_macro
+
+# Макрос для показа сообщения а диалоговом окне
+# message - адрес строки с сообщением для пользователя
+# type - тип сообщения
+.macro ShowMessage(%message, %type)
+	la 	a0 %message
+	mv 	a1 %type
+	li 	a7 55
+	ecall
+.end_macro
+
+# Макрос для ввода названия файла
+# message - адрес строкм  с сообщением для пользователя
+# address - адрес будущей строки 
+# size - размер вводимой строки с названием файла
+.macro InputFileName(%message, %address, %size) 
+	la 	a0 %message
+	la      a1 %address
+	li      a2 %size
+	li      a7 54
+	ecall
+.end_macro
+
+# Макрос, вызывающий подпрограмму для записи данных из файла 
+.macro ReadFile()
+	jal read_from_file
+.end_macro
+
+# Макрос, вызывающий подпрограмму для записи данных в файл
+.macro WriteFile()
+	jal out_in_file
 .end_macro
 
 # Макрос для выхода из программы
