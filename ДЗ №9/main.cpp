53342d69120708ab752070395d1df9d647017c81
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2023-12-13 15:49:05 +0300

    Update main.cpp

--- ДЗ №9/main.cpp
+++ ДЗ №9/main.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <random>
+#include <iostream>
 
 // Размер буфера
 #define BUFFER_SIZE 50
@@ -24,9 +25,45 @@ int summators = 0;
 // Окончательный результат
 int result = 0;
 
+// Инициализация функций
+void* sumThread(void* args);
+void* observeThread(void* arg);
+void* generatorThread(void* arg);
+
+int main() {
+    // Инициализация мьютекса и условной переменной
+    pthread_mutex_init(&bufferMutex, NULL);
+    pthread_cond_init(&bufferCond, NULL);
+
+    // Создание потока отслеживания буфера
+    pthread_t observer;
+    pthread_create(&observer, NULL, observeThread, NULL);
+
+    // Создание потоков генераторов
+    for (int i = 1; i <= THREADS_NUMBER; ++i) {
+        pthread_t thread;
+        int* number = new int;
+        *number = i;
+
+        // Запуск потока генератора
+        pthread_create(&thread, NULL, generatorThread, number);
+    }
+
+    // Ожидание завершения потока отслеживания буфера
+    pthread_join(observer, NULL);
+
+    // Уничтожение мьютекса и условной переменной
+    pthread_mutex_destroy(&bufferMutex);
+    pthread_cond_destroy(&bufferCond);
+
+    // Вывод окончательного результата
+    printf("Final result: %d", result);
+
+    return 0;
+}
+
 // Функция для суммирования чисел с задержкой
 void* sumThread(void* args) {
-    // Генерация случайной задержки от 3 до 5 секунд
     sleep(rand() % 3 + 3);
 
     pthread_mutex_lock(&bufferMutex);
@@ -34,12 +71,34 @@ void* sumThread(void* args) {
     // Получение чисел для суммирования
     int* numbers = (int*) args;
     int sum = numbers[0] + numbers[1];
-    printf("SumThread summed numbers %d, %d and sent result %d to buffer.\n", numbers[0], numbers[1], sum);
+    std::cout << "SumThread summed numbers {" << numbers[0] << ", " << numbers[1] << "} and sent result " << sum << " to buffer" << std::endl;
 
     // Помещение результата суммирования в буфер
     buffer[currentNumber++] = sum;
     // Уменьшение счетчика активных сумматоров
-    --summators;
+    summators--;
+
+    // Сигнализация об изменении буфера
+    pthread_cond_signal(&bufferCond);
+    pthread_mutex_unlock(&bufferMutex);
+
+    return NULL;
+}
+
+// Функция для генерации случайного числа с задержкой
+void* generatorThread(void* arg) {
+    sleep(rand() % 7 + 1);
+
+    pthread_mutex_lock(&bufferMutex);
+
+    // Генерация случайного числа от 1 до 20
+    int number = rand() % 20 + 1;
+    std::cout << "Generator Thread sent number " << number << " to buffer\n";
+
+    // Помещение числа в буфер
+    buffer[currentNumber++] = number;
+    // Уменьшение счетчика активных генераторов
+    generators--;
 
     // Сигнализация об изменении буфера
     pthread_cond_signal(&bufferCond);
@@ -73,7 +132,6 @@ void* observeThread(void* arg) {
             // Запускаем поток сумматора
             pthread_t adder;
             pthread_create(&adder, NULL, sumThread, numbers);
-            pthread_detach(adder);
         } else if (generators == 0 && summators == 0) {
             // Завершаем работу, если генераторы и сумматоры завершились
             result = buffer[0];
@@ -87,68 +145,4 @@ void* observeThread(void* arg) {
             pthread_mutex_unlock(&bufferMutex);
         }
     }
-}
-
-// Функция для генерации случайного числа с задержкой
-void* generateNumber(void* arg) {
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dis(1, 7);
-
-    // Генерация случайной задержки от 1 до 2 секунд
-    sleep(dis(gen));
-
-    pthread_mutex_lock(&bufferMutex);
-
-    // Генерация случайного числа от 1 до 20
-    int number = rand() % 20 + 1;
-    printf("Generator Thread %d sent number %d to buffer.\n", currentNumber, number);
-
-    // Помещение числа в буфер
-    buffer[currentNumber++] = number;
-    // Уменьшение счетчика активных генераторов
-    --generators;
-
-    // Сигнализация об изменении буфера
-    pthread_cond_signal(&bufferCond);
-    pthread_mutex_unlock(&bufferMutex);
-
-    return NULL;
-}
-
-int main() {
-    // Инициализация мьютекса и условной переменной
-    pthread_mutex_init(&bufferMutex, NULL);
-    pthread_cond_init(&bufferCond, NULL);
-
-    // Создание потока отслеживания буфера
-    pthread_t observer;
-    pthread_create(&observer, NULL, observeThread, NULL);
-
-    // Создание потоков генераторов
-    for (int i = 1; i <= THREADS_NUMBER; ++i) {
-        pthread_t thread;
-        int* number = new int;
-        *number = i;
-
-        // Запуск потока генератора
-        pthread_create(&thread, NULL, generateNumber, number);
-        pthread_detach(thread);
-    }
-
-    // Ожидание завершения потока отслеживания буфера
-    pthread_join(observer, NULL);
-
-    // Очистка мьютекса
-    pthread_mutex_lock(&bufferMutex);
-    pthread_mutex_unlock(&bufferMutex);
-
-    // Уничтожение мьютекса и условной переменной
-    pthread_mutex_destroy(&bufferMutex);
-    pthread_cond_destroy(&bufferCond);
-
-    // Вывод окончательного результата
-    printf("Final result: %d", result);
-
-    return EXIT_SUCCESS;
 }
